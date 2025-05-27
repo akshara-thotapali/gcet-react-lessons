d01d6cb759ba8dba9b858bdc58971329b1603882
akshara-thotapali <thotapalliakshara2@gmail.com>
2025-05-27 18:49:20 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,15 @@ export default function Header() {
       -<Link to="/app6">App6</Link>
       -<Link to="/app7">App7</Link>
       -<Link to="/app8">App8</Link>
+      -<Link to="/app9">App9</Link>
+      -<Link to="/app10">App10</Link>
+      -<Link to="/app11">App11</Link>
+      -<Link to="/app12">App12</Link>
+      -<Link to="/app13">App13</Link>
+      -<Link to="/app14">App14</Link>
+      -<Link to="/app15">App15</Link>
+      -<Link to="/app16">App16</Link>
+      -<Link to="/app17">App17</Link>
     </div>
   );
 }
